7af0618da0e43ffdaffec3a7e8c441ec781bdb93
Manasa G <manasa.g7890@hotmail.com>
2023-08-21 14:02:36 +0530

    updated quiz app

--- quizapp.js/script.js
+++ quizapp.js/script.js
@@ -33,4 +33,32 @@ const quizData = [
 ]
 
 let currentQuestion = 0;
+const questionEl = document.getElementById('question')
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById('submit')
+
 loadQuiz()
+
+function loadQuiz(){
+    const currentQuizData = quizData[currentQuestion];
+    questionEl.innerText=currentQuizData['question'];
+
+    a_text.innerText = currentQuestion.a;
+    b_text.innerText = currentQuestion.b ;
+    c_text.innerText = currentQuestion.c;
+    d_text.innerText = currentQuestion.d;
+
+   
+}
+submitBtn.addEventListener('click',()=>{
+    currentQuestion++;   
+
+    if(currentQuestion<quizData.length){
+        loadQuiz();
+    } else {
+        alert("You have completed the test")
+    }
+})
