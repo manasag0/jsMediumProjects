67b64a63eb44bb869b6c3c6b195fe7968c1d8a92
Manasa G <manasa.g7890@hotmail.com>
2023-08-21 17:08:57 +0530

    updated

--- quizapp.js/script.js
+++ quizapp.js/script.js
@@ -32,33 +32,62 @@ const quizData = [
     }
 ]
 
-let currentQuestion = 0;
-const questionEl = document.getElementById('question')
+const questionEl = document.getElementById('question');
+const answerEles = document.querySelectorAll(".answer")
 const a_text = document.getElementById('a_text');
 const b_text = document.getElementById('b_text');
 const c_text = document.getElementById('c_text');
 const d_text = document.getElementById('d_text');
-const submitBtn = document.getElementById('submit')
+const submitBtn = document.getElementById('submit');
+const quiz = document.getElementById('quiz')
+
+let currentQuestion = 0;
+let score = 0;
 
 loadQuiz()
 
 function loadQuiz(){
+    selectedAnswers()
     const currentQuizData = quizData[currentQuestion];
     questionEl.innerText=currentQuizData['question'];
 
-    a_text.innerText = currentQuestion.a;
-    b_text.innerText = currentQuestion.b ;
-    c_text.innerText = currentQuestion.c;
-    d_text.innerText = currentQuestion.d;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
 
    
 }
+
+function getSelected(){
+
+    let answer = undefined
+    answerEles.forEach((answersEl)=>{
+        if(answersEl.checked){
+           return answer = answersEl.id
+        }
+    }) 
+    return answer;
+}  
+
+function selectedAnswers(){
+ answerEles.forEach((answersEl)=>{
+    answersEl.checked = false
+ })
+}
+
 submitBtn.addEventListener('click',()=>{
-    currentQuestion++;   
+    currentQuestion++;  
+    
+    const answer = getSelected();
+    if(answer){
+        currentQuestion++
+   console.log(answer)
 
     if(currentQuestion<quizData.length){
         loadQuiz();
     } else {
-        alert("You have completed the test")
+        quiz.innerHTML = `<h2>You answered ${score}/${quizData.length}</h2>`
+    }
     }
 })
